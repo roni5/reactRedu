b314c35b72dfdcc69531e740867c83be87a594c1
“Roni” <roniemail1@gmail.com>
2018-07-13 15:03:48 +0100

    reduxmap inputwot

---
+++ src/components/input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+//import PropTypes from 'prop-types'
+
+//export default (props) => {}
+function input(props) {
+  return (
+    <div>
+      <input value={props.inputValue} onChange={props.inputChanged} />
+      <p>{props.inputValue}</p>
+    </div>
+  );
+}
+//input.propTypes = {
+//}
+//reduxmap better hocredux
+//const mapStateToProps = (state) => ({
+//  inputValue: state.inputValue
+//})
+const mapStateToProps = (state) => {
+  return {
+    inputValue: state.inputValue
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    inputChanged: (evt) => {
+      console.log('changed', evt.target.value);
+      const action = {type: 'INPUT_CHANGE' , text: evt.target.value}
+      dispatch(action);
+     }
+   }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(input);
